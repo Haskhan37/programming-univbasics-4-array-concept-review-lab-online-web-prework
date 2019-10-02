a1ae00b453e903a291663416fdd332bd7ebcdca3
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-02 20:10:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -39,5 +39,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-    x = array
+    x = array[0]
+    array.length.times do |index|
+      if array[index] < x 
+        x = array[index]
+      end
+    end
+    x
 end
